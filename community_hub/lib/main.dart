6a39b246de269965e2d88f943879d59a81f6932e
enoch-aik <enoch_aik@icloud.com>
2023-11-22 00:36:49 +0100

    - added firebase dependencies

--- community_hub/lib/main.dart
+++ community_hub/lib/main.dart
@@ -1,8 +1,27 @@
 import 'package:community_hub/lib.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
+late final FirebaseApp app;
+late final FirebaseAuth auth;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //set app orientation to only portrait view
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  //Initialize FirebaseApp and FirebaseAuth into app
+  app = await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  auth = FirebaseAuth.instanceFor(app: app);
   runApp(const ProviderScope(child: MyApp()));
 }
