aa5c16e48c451c4127ca8414b70a2d0eb16cc566
enoch-aik <enoch_aik@icloud.com>
2023-11-22 13:32:35 +0100

    - added local notification handler

--- community_hub/lib/main.dart
+++ community_hub/lib/main.dart
@@ -1,10 +1,32 @@
 import 'package:community_hub/lib.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'app.dart';
 import 'firebase_options.dart';
+
+//background notification handler
+@pragma('vm:entry-point')
+Future<void> _backgroundHandler(RemoteMessage message) async {
+  //await Firebase.initializeApp();
+  FlutterLocalNotificationsPlugin notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  await notificationsPlugin.show(
+      1,
+      message.notification!.title,
+      message.notification!.body,
+      const NotificationDetails(
+          android: AndroidNotificationDetails('noti', 'noti',
+              priority: Priority.max,
+              importance: Importance.max,
+              enableVibration: true,
+              // sound: RawResourceAndroidNotificationSound('bell_tick'),
+              // icon: '@mipmap/ic_launcher',
+              channelDescription: 'noti')));
+}
 
 late final FirebaseApp app;
 late final FirebaseAuth auth;
